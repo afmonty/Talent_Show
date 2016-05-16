4d26cd2847553925dc05e7170f47b4ea7b922f58
anna hunter <annfmonty@gmail.com>
2016-05-16 09:50:06 -0500

    more styling

--- public/scripts/components/Home.js
+++ public/scripts/components/Home.js
@@ -8,7 +8,7 @@ export default React.createClass({
 			<main className = 'homepage'>
 				<Nav className = 'homeNav'/>
 				<div className = 'display'>
-					<h1 className = 'greeting'>Welcome to the Talent Show</h1>
+					<h2 className = 'greeting'>Welcome to the Talent Show</h2>
 					<div className = 'welcome'>
 						What's your talent?  Are you a dancer, a singer?  Do you love to play football?  You want your talent to take you places, but how?  Finding the right college program can help you be Excellent, and Talent Show can help you get there.  With Talent Show, you can select a college or Univeristy and submit a video of you at your best!  Welcome to your Talent Show!  
 					</div>
